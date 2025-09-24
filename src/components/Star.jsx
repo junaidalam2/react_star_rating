8590c89ff30c00998ca8d899a470a23307406a43
junaidalam2 <junaid.alam2@gmail.com>
2025-09-23 17:37:03 -0700

    initial commit

---
+++ src/components/Star.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+function Star({ rating, setRating }) {
+
+    const star_arr = [1, 2, 3, 4, 5]
+    const [hover, setHover] = useState(0);
+
+    return (
+        <div>
+            {star_arr.map((star) => {
+                return (
+                    <span
+                        className='start'
+                        style={{
+                            cursor: 'pointer',
+                            color: (hover || rating) >= star ? 'gold' : 'gray',
+                            fontSize: `35px`,
+                            transition: "color 0.2s ease-in-out",
+                        }}
+                        onClick={() => { setRating(star) }}
+                        onMouseEnter={() => setHover(star)}
+                        onMouseLeave={() => setHover(0)}
+                    >
+                        {' '}
+                        ★{' '}
+                    </span>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Star;
